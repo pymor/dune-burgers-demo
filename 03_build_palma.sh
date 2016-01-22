a9c86c685964b6fe38ce238381afec05b22e057f
Stephan Rave <stephanrave@uni-muenster.de>
2016-01-22 14:35:18 +0100

    fix dunecontrol path in 03_build_palma.sh

--- 03_build_palma.sh
+++ 03_build_palma.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-./dune-common/bin/dunecontrol --opts=palma.opts --module=dune-burgers all
+./dune-common-2.3.1/bin/dunecontrol --opts=palma.opts --module=dune-burgers all
 
 # rebuild dune-burgers with --enable-shared
 
 touch dune-burgers/src/dune_burgers.cc
-./dune-common/bin/dunecontrol --opts=dune-burgers-palma.opts --only=dune-burgers all
+./dune-common-2.3.1/bin/dunecontrol --opts=dune-burgers-palma.opts --only=dune-burgers all
 
